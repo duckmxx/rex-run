506e552dd812e7e4a31c1d61ad5e5a19b1e985c0
duckmxx <noobster611@gmail.com>
2026-06-06 22:19:29 -0700

    cactus accelerates and decelerates, drawing thing fixed now dino should be visible at all times, and added text in menu. need to make a struct because theres a million arguments inside of drawgame.

--- main.cpp
+++ main.cpp
@@ -59,6 +59,7 @@ class Dino {
 class Cactus {
     private:
         float velocity;
+        float starting_velocity;
         Vector2 position;
         Vector2 starting_pos;
         float width;
@@ -67,8 +68,9 @@ class Cactus {
 
 
     public:
-        Cactus(float velocity, Vector2 startPos, float width, float height, Color color)
-            : velocity{velocity},
+        Cactus(float starting_velocity, Vector2 startPos, float width, float height, Color color)
+            : velocity{starting_velocity},
+              starting_velocity{starting_velocity},
               position{startPos},
               starting_pos(startPos),
               width(width),
@@ -76,7 +78,8 @@ class Cactus {
               color{color}
         {}
 
-        void update(float dt) {
+        void update(float dt, float accel) {
+            velocity += accel * dt;
             position.x -= velocity * dt;
             if ((position.x + width) <= 0) {
                 position.x = starting_pos.x;
@@ -93,6 +96,11 @@ class Cactus {
             };
         }
 
+        void reset() {
+            velocity = starting_velocity;
+            position = starting_pos;
+        }
+
         void draw() const {
             DrawRectangle(position.x, position.y, width, height, color);
         }
@@ -105,7 +113,7 @@ enum class GameState {
     GameOver
 };
 
-void update_game(GameState& game_state, Dino& dino, Cactus& cactus, float dt, float gravity) {
+void update_game(GameState& game_state, Dino& dino, Cactus& cactus, float dt, float gravity, float accel) {
     switch(game_state) {
         
         case GameState::Menu:
@@ -116,40 +124,45 @@ void update_game(GameState& game_state, Dino& dino, Cactus& cactus, float dt, fl
         
         case GameState::Playing:
             dino.update(dt, gravity);
-            cactus.update(dt);
+            cactus.update(dt, accel);
             if (CheckCollisionRecs(dino.get_rect(), cactus.get_rect())) {
                 game_state = GameState::GameOver;
-            }
-            if (IsKeyPressed(KEY_R)) {
-                game_state = GameState::Playing;
             }
             break;
         
         case GameState::GameOver:
-            if (IsKeyPressed(KEY_R)) {
+            if (IsKeyPressed(KEY_SPACE)) {
+                cactus.reset();
                 game_state = GameState::Playing;
             }
             break;
         }
 }
-void draw_game(GameState game_state, const Dino& dino, const Cactus& cactus) {
+void draw_game(GameState game_state, const Dino& dino, const Cactus& cactus, int title_font_size, int title_text_length, int fun_font_size, int fun_text_length) {
     switch(game_state) {
 
         case GameState::Menu:
+            DrawText("Rex Run", (SCREEN_WIDTH - title_text_length) / 2, 50, title_font_size, BLACK);
+            DrawText("Press SPACE to run!", (SCREEN_WIDTH - fun_text_length) / 2, 100, fun_font_size, BLACK);
+            dino.draw();
+            cactus.draw();
             break;
         case GameState::Playing:
             dino.draw();
             cactus.draw();
             break;
         case GameState::GameOver:
+            dino.draw();
+            cactus.draw();
             break;
     }
 }
 
-
-float gravity = 1500.0f;
+float accel = 10.0f;
+float gravity = 2000.0f;
 Vector2 dino_start_pos = {180.0f, 500.0f};
 Vector2 dino_start_velocity = {0.0f, 0.0f};
+Vector2 cactus_start_pos = {1280, GROUND_Y - 75};
 
 int main() {
     
@@ -157,21 +170,25 @@ int main() {
     SetTargetFPS(60);
     
     Dino dino(dino_start_velocity, dino_start_pos, 50, 100, true, BLACK);
-    Cactus cacti(450, {1280, GROUND_Y - 75}, 25, 75, GREEN);
+    Cactus cacti(450.0f, cactus_start_pos, 25, 75, GREEN);
     
     GameState game_state = GameState::Menu;
 
+    int title_font_size = 50;
+    int title_text_length = MeasureText("Rex Run", 50);
+    int fun_font_size = 30;
+    int fun_text_length = MeasureText("Press SPACE to run!", 30);
 
     while(!WindowShouldClose()) {
         float dt = GetFrameTime();
         
-        update_game(game_state, dino, cacti, dt, gravity);
+        update_game(game_state, dino, cacti, dt, gravity, accel);
 
 
         BeginDrawing();
             ClearBackground(GRAY);
             DrawLine(0, GROUND_Y, SCREEN_WIDTH, GROUND_Y, WHITE);
-            draw_game(game_state, dino, cacti);
+            draw_game(game_state, dino, cacti, title_font_size, title_text_length, fun_font_size, fun_text_length);
 
         EndDrawing();
     }
